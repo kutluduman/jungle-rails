bf84444758284aed2d0fda109a8d5193be547586
Kutluhan Duman <kutluduman@gmail.com>
2020-09-22 02:39:56 +0000

    Changed the expectation of the test to count 10 instead of all products

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -22,9 +22,9 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     visit root_path
 
     # DEBUG / VERIFY
-    save_screenshot
-    
-    expect(page).to have_css "article.product"
+    #save_screenshot
+
+     expect(page).to have_css "article.product", count: 10
   end
 
 end
